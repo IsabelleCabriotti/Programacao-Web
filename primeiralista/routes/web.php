ce1ee4f4698521132ac8fc306cc5e5bbcf01b090
IsabelleCabriotti <isahcabriotti@gmail.com>
2025-02-25 20:11:50 -0300

    Atualização com o nome da pasta

--- primeiralista/routes/web.php
+++ primeiralista/routes/web.php
@@ -23,165 +23,165 @@ Route::post('/exer1resp', function(Request $request){
 });
 
 Route::get('/ex1', function(){
-    return view('lista.ex1');
+    return view('lista1.ex1');
 });
 
-Route::post('listaex1', function(Request $request){
+Route::post('lista1ex1', function(Request $request){
     $nota1 = floatval($request->input('nota1'));
     $nota2 = floatval($request->input('nota2'));
     $nota3 = floatval($request->input('nota3'));
     $media = ($nota1 + $nota2 + $nota3) / 3;
-    return view('lista.ex1', compact('media'));
+    return view('lista1.ex1', compact('media'));
 });
 
 Route::get('/ex2', function(){
-    return view('lista.ex2');
+    return view('lista1.ex2');
 });
 
-Route::post('listaex2', function(Request $request){
+Route::post('lista1ex2', function(Request $request){
     $celsius = intval($request->input('celsius'));
     $fahrenheit = ($celsius * 9/5) + 32;
-    return view('lista.ex2', compact('fahrenheit'));
+    return view('lista1.ex2', compact('fahrenheit'));
 });
 
 Route::get('/ex3', function(){
-    return view('lista.ex3');
+    return view('lista1.ex3');
 });
 
-Route::post('listaex3', function(Request $request){
+Route::post('lista1ex3', function(Request $request){
     $fahrenheit = floatval($request->input('fahrenheit'));
     $celsius = ($fahrenheit - 32) * 5/9;
-    return view('lista.ex3', compact('celsius'));
+    return view('lista1.ex3', compact('celsius'));
 });
 
 Route::get('/ex4', function(){
-    return view('lista.ex4');
+    return view('lista1.ex4');
 });
 
-Route::post('listaex4', function(Request $request){
+Route::post('lista1ex4', function(Request $request){
     $largura = floatval($request->input('largura'));
     $altura = floatval($request->input('altura'));
     $area = $largura * $altura;
-    return view('lista.ex4', compact('area'));
+    return view('lista1.ex4', compact('area'));
 });
 
 Route::get('/ex5', function(){
-    return view('lista.ex5');
+    return view('lista1.ex5');
 });
 
-Route::post('listaex5', function(Request $request){
+Route::post('lista1ex5', function(Request $request){
     $raio = floatval($request->input('raio'));
     $area = pi() * pow($raio, 2);
-    return view('lista.ex5', compact('area'));
+    return view('lista1.ex5', compact('area'));
 });
 
 Route::get('/ex6', function(){
-    return view('lista.ex6');
+    return view('lista1.ex6');
 });
 
-Route::post('listaex6', function(Request $request){
+Route::post('lista1ex6', function(Request $request){
     $largura = floatval($request->input('largura'));
     $altura = floatval($request->input('altura'));
     $perimetro = 2 * ($largura + $altura);
-    return view('lista.ex6', compact('perimetro'));
+    return view('lista1.ex6', compact('perimetro'));
 });
 
 Route::get('/ex7', function(){
-    return view('lista.ex7');
+    return view('lista1.ex7');
 });
 
-Route::post('listaex7', function(Request $request){
+Route::post('lista1ex7', function(Request $request){
     $raio = floatval($request->input('raio'));
     $perimetro = 2 * pi() * $raio;
-    return view('lista.ex7', compact('perimetro'));
+    return view('lista1.ex7', compact('perimetro'));
 });
 
 Route::get('/ex8', function(){
-    return view('lista.ex8');
+    return view('lista1.ex8');
 });
 
-Route::post('listaex8', function(Request $request){
+Route::post('lista1ex8', function(Request $request){
     $expoente = floatval($request->input('expoente'));
     $base = floatval($request->input('base'));
     $resultado = pow($base, $expoente);
-    return view('lista.ex8', compact('resultado'));
+    return view('lista1.ex8', compact('resultado'));
 });
 
 Route::get('/ex9', function(){
-    return view('lista.ex9');
+    return view('lista1.ex9');
 });
 
-Route::post('listaex9', function(Request $request){
+Route::post('lista1ex9', function(Request $request){
     $metros = floatval($request->input('metros'));
     $centimetros = $metros * 100;
-    return view('lista.ex9', compact('centimetros'));
+    return view('lista1.ex9', compact('centimetros'));
 });
 
 Route::get('/ex10', function(){
-    return view('lista.ex10');
+    return view('lista1.ex10');
 });
 
-Route::post('listaex10', function(Request $request){
+Route::post('lista1ex10', function(Request $request){
     $quilometros = floatval($request->input('quilometros'));
     $milhas = $quilometros * 0.621371;
-    return view('lista.ex10', compact('milhas'));
+    return view('lista1.ex10', compact('milhas'));
 });
 
 Route::get('/ex11', function(){
-    return view('lista.ex11');
+    return view('lista1.ex11');
 });
 
-Route::post('listaex11', function(Request $request){
+Route::post('lista1ex11', function(Request $request){
     $peso = floatval($request->input('peso'));
     $altura = floatval($request->input('altura'));
     $imc = $peso / ($altura * $altura);
-    return view('lista.ex11', compact('imc'));
+    return view('lista1.ex11', compact('imc'));
 });
 
 Route::get('/ex12', function(){
-    return view('lista.ex12');
+    return view('lista1.ex12');
 });
 
-Route::post('listaex12', function(Request $request){
+Route::post('lista1ex12', function(Request $request){
     $preco = floatval($request->input('preco'));
     $desconto = floatval($request->input('desconto'));
     $valor_desconto = ($preco * $desconto) / 100;
     $preco_final = $preco - $valor_desconto;
-    return view('lista.ex12', compact('preco_final'));
+    return view('lista1.ex12', compact('preco_final'));
 });
 
 Route::get('/ex13', function(){
-    return view('lista.ex13');
+    return view('lista1.ex13');
 });
 
-Route::post('listaex13', function(Request $request){
+Route::post('lista1ex13', function(Request $request){
     $capital = floatval($request->input('capital'));
     $taxa = floatval($request->input('taxa'));
     $periodo = intval($request->input('periodo'));
     $juros = ($capital * $taxa * $periodo) / 100;
-    return view('lista.ex13', compact('juros'));
+    return view('lista1.ex13', compact('juros'));
 });
 
 Route::get('/ex14', function(){
-    return view('lista.ex14');
+    return view('lista1.ex14');
 });
 
-Route::post('listaex14', function(Request $request){
+Route::post('lista1ex14', function(Request $request){
     $capital = floatval($request->input('capital'));
     $taxa = floatval($request->input('taxa'));
     $periodo = intval($request->input('periodo'));
     $juros = $capital * pow((1 + $taxa), $periodo);
-    return view('lista.ex14', compact('juros'));
+    return view('lista1.ex14', compact('juros'));
 });
 
 Route::get('/ex15', function(){
-    return view('lista.ex15');
+    return view('lista1.ex15');
 });
 
-Route::post('listaex15', function(Request $request){
+Route::post('lista1ex15', function(Request $request){
     $dias = intval($request->input('dias'));
     $horas = $dias * 24;
     $minutos = $horas * 60;
     $segundos = $minutos * 60;
-    return view('lista.ex15', compact('dias', 'horas', 'minutos', 'segundos'));
+    return view('lista1.ex15', compact('dias', 'horas', 'minutos', 'segundos'));
 });
